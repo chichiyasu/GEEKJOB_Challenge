8797b8ee5b7dfba39cad88548dd52645728b1424
inagaki <goldship1222.y.i@gmail.com>
2018-03-22 17:40:28 +0900

    クラスの継承　課題を提出させて頂きます。

---
+++ CampChallenge_java/013.クラス/class2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classpackkage;
+
+/**
+ *
+ * @author inagakiyuuichi
+ */
+public class class2 extends Human {
+
+    public void method1() {
+
+        name = "null";
+        age = 0;
+
+    }
+
+}
